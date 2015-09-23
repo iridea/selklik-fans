ef36dd409f91ca6cbdbb18befffdd0f421b41821
Jamal N. Ahmad <jamal.nasir@me.com>
2015-09-23 16:37:43 +0800

    init project

---
+++ Selklik-Fans/Selklik-Fans/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Selklik-Fans
+//
+//  Created by Jamal N. Ahmad on 9/23/15.
+//  Copyright © 2015 Selklik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
