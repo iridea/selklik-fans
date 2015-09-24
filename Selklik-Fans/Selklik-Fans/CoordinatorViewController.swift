44a98a3f0521bdf3eff27f7d59ca3bd8d5f947ec
Jamal N. Ahmad <jamal.nasir@me.com>
2015-09-24 12:29:41 +0800

    Add Coordinator View Controller interface

---
+++ Selklik-Fans/Selklik-Fans/CoordinatorViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  Selklik-Fans
+//
+//  Created by Jamal N. Ahmad on 9/23/15.
+//  Copyright © 2015 Selklik. All rights reserved.
+//
+
+import UIKit
+
+class CoordinatorViewController: UIViewController {
+
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var joinButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        //loginButton.backgroundColor = UIColor.clearColor()
+        loginButton.layer.cornerRadius = 10
+        loginButton.layer.borderWidth = 1
+        loginButton.layer.borderColor = UIColor.whiteColor().CGColor
+        
+        //joinButton.backgroundColor = UIColor.clearColor()
+        joinButton.layer.cornerRadius = 10
+        joinButton.layer.borderWidth = 1
+        joinButton.layer.borderColor = UIColor.whiteColor().CGColor
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
