7cb8a893dd364776802abb26f720f8d701db6ae0
Jamal N. Ahmad <jamal.nasir@me.com>
2015-09-26 08:47:08 +0800

    finished login with HUD

--- Selklik-Fans/Selklik-Fans/LoginViewController.swift
+++ Selklik-Fans/Selklik-Fans/LoginViewController.swift
@@ -19,6 +19,10 @@ class LoginViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     
+    var messageFrame = UIView()
+    var activityIndicator = UIActivityIndicatorView()
+    var strLabel = UILabel()
+    
     //MARK: - Custom Function
     @IBAction func backButtonPress(sender: AnyObject) {
         self.dismissViewControllerAnimated(true, completion: nil)
@@ -39,6 +43,8 @@ class LoginViewController: UIViewController, UITextFieldDelegate {
     
     private func loginToSystem() {
         
+        progressBarDisplayer("Verifying...", true)
+        
         let username = self.emailTextField.text
         let password = self.passwordTextField.text
         
@@ -53,13 +59,43 @@ class LoginViewController: UIViewController, UITextFieldDelegate {
             switch result {
             case .Success:
                 print("Validation Successful")
+                 self.messageFrame.removeFromSuperview()
                 let json = JSON(result.value!)
                 print(json["token"].string!)
             case .Failure(_, let error):
                 print(error)
             }
         }
+        //hudView.hidden = true
+        //self.dismissViewControllerAnimated(true, completion: nil)
     }
+    
+    func progressBarDisplayer(msg:String, _ indicator:Bool ) {
+        
+        let hudView = UIView(frame: view.bounds)
+        hudView.opaque = false
+        
+        view.addSubview(hudView);
+        view.userInteractionEnabled = false
+        hudView.opaque = false
+        hudView.backgroundColor = UIColor(red: 42/255, green: 42/255, blue: 42/255, alpha: 0.5)
+        
+        strLabel = UILabel(frame: CGRect(x: 50, y: 0, width: 200, height: 50))
+        strLabel.text = msg
+        strLabel.textColor = UIColor.whiteColor()
+        messageFrame = UIView(frame: CGRect(x: view.frame.midX - 90, y: view.frame.midY - 50 , width: 180, height: 50))
+        messageFrame.layer.cornerRadius = 15
+        messageFrame.backgroundColor = UIColor(white: 0, alpha: 0.7)
+        if indicator {
+            activityIndicator = UIActivityIndicatorView(activityIndicatorStyle: UIActivityIndicatorViewStyle.White)
+            activityIndicator.frame = CGRect(x: 0, y: 0, width: 50, height: 50)
+            activityIndicator.startAnimating()
+            messageFrame.addSubview(activityIndicator)
+        }
+        messageFrame.addSubview(strLabel)
+        view.addSubview(messageFrame)
+    }
+    
     @IBAction func loginButtonTouchUpInside(sender: AnyObject) {
         loginToSystem()
     }
