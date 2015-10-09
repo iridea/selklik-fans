4ff1df588ab64647fe6ac70c18f7d0fd9546bb6b
Jamal N. Ahmad <jamal.nasir@me.com>
2015-10-09 17:57:02 +0800

    add logout function. - helper function for getting UserToken from core data - helper function for clearing user access data in core data

--- Selklik-Fans/Selklik-Fans/Controllers/SlideMenuViewController.swift
+++ Selklik-Fans/Selklik-Fans/Controllers/SlideMenuViewController.swift
@@ -7,8 +7,22 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
 
 class SlideMenuViewController: UIViewController {
+
+    let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+
+    //variable for loading HUD
+    var messageFrame = UIView()
+    var activityIndicator = UIActivityIndicatorView()
+    var strLabel = UILabel()
+
+    @IBAction func logoutButton(sender: AnyObject) {
+        logoutFromSystem()
+
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,14 +36,83 @@ class SlideMenuViewController: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
+    //MARK: - Custom Function
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func progressBarDisplayer(msg:String, _ indicator:Bool ) {
+
+        view.userInteractionEnabled = false
+
+        strLabel = UILabel(frame: CGRect(x: 50, y: 0, width: 200, height: 50))
+        strLabel.text = msg
+        strLabel.textColor = UIColor.whiteColor()
+        messageFrame = UIView(frame: CGRect(x: view.frame.midX - 90, y: view.frame.midY - 50 , width: 180, height: 50))
+        messageFrame.layer.cornerRadius = 15
+        messageFrame.backgroundColor = UIColor(white: 0, alpha: 0.7)
+        if indicator {
+            activityIndicator = UIActivityIndicatorView(activityIndicatorStyle: UIActivityIndicatorViewStyle.White)
+            activityIndicator.frame = CGRect(x: 0, y: 0, width: 50, height: 50)
+            activityIndicator.startAnimating()
+            messageFrame.addSubview(activityIndicator)
+        }
+        messageFrame.addSubview(strLabel)
+        view.addSubview(messageFrame)
     }
-    */
+
+    private func logoutFromSystem() {
+
+        progressBarDisplayer("Logout...", true)
+
+        let headers = ["Content-Type": "application/x-www-form-urlencoded"]
+
+        let userInfo = UserInfo()
+        let token = userInfo.getTokenFromCoreData(appDelegate.coreDataStack.context)
+        let parameters = ["token":token]
+
+        let postUrl = API.logoutUrl
+
+        Alamofire.request(.POST, postUrl, headers: headers, parameters: parameters).responseJSON { _, _, result in
+            switch result {
+            case .Success:
+
+                self.messageFrame.removeFromSuperview()
+
+                let json = JSON(result.value!)
+
+                print("json from logout:\(json)")
+
+                if  (json["status"].boolValue){
+
+                    print("Logout Successful")
+
+                    userInfo.removeUserAccessFromCoreData(self.appDelegate.coreDataStack.context)
+
+                    dispatch_async(dispatch_get_main_queue()) {
+
+                        self.messageFrame.removeFromSuperview()
+                        self.view.userInteractionEnabled = true
+                        let storyboard = UIStoryboard(name: "Startup", bundle: nil)
+                        let controller = storyboard.instantiateViewControllerWithIdentifier("InitialStartup") as! CoordinatorViewController
+                        controller.managedContext = self.appDelegate.coreDataStack.context
+                        self.presentViewController(controller, animated: true, completion: nil)
+                    }
+                }
+                else{
+                    print("Problem with logout")
+                    let uiAlert = UIAlertController(title: Title.logoutFail, message: Message.logoutFail, preferredStyle: UIAlertControllerStyle.Alert)
+                    self.presentViewController(uiAlert, animated: true, completion: nil)
+
+                    uiAlert.addAction(UIAlertAction(title: "Ok", style: .Default, handler: { action in
+                        self.messageFrame.removeFromSuperview()
+                        self.view.userInteractionEnabled = true
+
+                    }))
+
+                }
+            case .Failure(_, let error):
+                
+                print(error)
+            }
+        }
+    }
 
 }
--- Selklik-Fans/Selklik-Fans/Helpers/UserInfo.swift
+++ Selklik-Fans/Selklik-Fans/Helpers/UserInfo.swift
@@ -7,3 +7,54 @@
 //
 
 import Foundation
+import CoreData
+
+class UserInfo {
+    func getTokenFromCoreData(managedContext: NSManagedObjectContext) -> String{
+
+        var userToken:String!
+        let accessFetch = NSFetchRequest(entityName: "Access")
+        do  {
+            let result = try managedContext.executeFetchRequest(accessFetch) as? [SelklikFansAccess]
+
+            if let loginUser = result {
+
+                //if old token exist in core data, delete it
+                if loginUser.count > 0 {
+                    userToken  = loginUser[0].token
+
+                }
+
+            }
+        }
+        catch let fetchError as NSError {
+            print("dogFetch error: \(fetchError.localizedDescription)")
+        }
+
+        return userToken
+    }
+
+    func removeUserAccessFromCoreData(managedContext: NSManagedObjectContext){
+
+        let fetchPosts = NSFetchRequest(entityName: "Access")
+
+        do  {
+            let fetchedEntities = try managedContext.executeFetchRequest(fetchPosts) as? [SelklikFansAccess]
+
+            for entity in fetchedEntities! {
+                managedContext.deleteObject(entity)
+            }
+
+        }
+        catch let fetchError as NSError {
+            print("Fetch Post for delete error: \(fetchError.localizedDescription)")
+        }
+
+        do {
+            try managedContext.save()
+        } catch let error as NSError {
+            print("Could not save: \(error)")
+        }
+    }
+    
+}
